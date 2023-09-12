91a3df21ae8f3892e5d6fb44d11b259578f8a9b7
Gabi246823 <144746058+Gabi246823@users.noreply.github.com>
2023-09-12 20:02:16 -0300

    Update rankingRoute.js

--- src/routes/rankingRoute.js
+++ src/routes/rankingRoute.js
@@ -1,1 +1,9 @@
+import rankingController from '../controllers/rankingController.js';
 
+export default (app) => {
+  app.get('/getranking/:id_ranking', rankingController.getranking);
+  app.get('/getranking', rankingController.getranking);
+  app.post('/createranking', rankingController.createranking);
+  app.delete('/deleteranking/:id_ranking', rankingController.deleteranking);
+  app.post('/updateranking/:id_ranking', rankingController.updateranking);
+}
